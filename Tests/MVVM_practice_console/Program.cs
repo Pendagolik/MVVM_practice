8de9d46f5c7ba375de3700ed68dbce9e9d49db97
Илья Просветов <i_pros@mail.ru>
2022-09-26 16:37:40 +0300

    Тестовый консольный проект

---
+++ Tests/MVVM_practice_console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MVVM_practice_console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
