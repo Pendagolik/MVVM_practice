42b04e4117290e8d3704c8f9d517288f7a6b03b0
Илья Просветов <i_pros@mail.ru>
2022-10-01 19:07:48 +0300

    Базовый класс команды

--- MVVM_practice/Infrastructure/Commands/Base/Command.cs
+++ MVVM_practice/Infrastructure/Commands/Base/Command.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace MVVM_practice.Infrastructure.Commands.Base
 {
-    internal abstract class Command
+    internal abstract class Command : ICommand
     {
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+
+            remove => CommandManager.RequerySuggested -= value;
+        }
+        public abstract bool CanExecute(object parameter);
+        public abstract void Execute(object parameter);
     }
 }
