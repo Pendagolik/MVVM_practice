2d7ded2db37dab883747c815d4913b5ac8cae00c
Илья Просветов <i_pros@mail.ru>
2022-10-11 13:13:16 +0300

    Добавляем в окно-преджставления главного окна кнопку выхода из приложения

--- MVVM_practice/ViewModels/MainWindowViewModel.cs
+++ MVVM_practice/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,12 @@
-﻿using MVVM_practice.ViewModels.Base;
+﻿using MVVM_practice.Infrastructure.Commands;
+using MVVM_practice.ViewModels.Base;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace MVVM_practice.ViewModels
 {
@@ -39,5 +42,31 @@ namespace MVVM_practice.ViewModels
             set => Set(ref _Status, value);
         }
         #endregion
+
+        #region Команды
+
+        #region CloseApplicationCommand
+
+        public ICommand CloseApplicationCommand { get; }
+
+        private bool CanCloseApplicationCommandExecute(object p) => true;
+
+        private void OnCloseApplicationCommandExecuted(object p)
+        {
+            Application.Current.Shutdown();
+        }
+
+        #endregion
+
+        #endregion
+
+        public MainWindowViewModel()
+        {
+            #region Команды
+
+            CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
+
+            #endregion
+        }
     }
 }
